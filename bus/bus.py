081cc262d725d3ac0951769686f2ef9da603ae0d
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2023-12-28 23:53:23 -0500

    Update bus.py

--- bus/bus.py
+++ bus/bus.py
@@ -5,7 +5,7 @@ def main():
     for i in 8:
         first = input("first name: ")
         last = input("last name: ")
-        x = (input("grade level: ")
+        x = input("grade level: ")
         if x == 'k' or x == 'K':
           grade = ord(x) - ord(x)
         elif x == '1' or x == '2' or x == '3' or x == '4' or x == '5' or x == '6' or x == '7' or x == '8' or x == '9' or x == '10' or x == '11' or x = '12':
