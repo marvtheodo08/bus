a3471d501b9560cf70b8b484400f7c0198f8dbb2
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2024-02-24 17:41:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request
 import sqlite3
 
-#configure app
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def homepage():
   if request.method == "POST":  
-  
-  #render template
   return render_template("index.html")
 
                          
