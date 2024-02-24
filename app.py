2535cb4c93dca66f7b676b1a168ea61b9a666332
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2024-02-24 17:00:50 -0500

    Update app.py
    swithced methods from homeage from GET and POST to just POST because the user is just submitting info to the server.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
 
-@app.route('/', methods=['GET','POST])
+@app.route('/', methods=['POST])
 def homepage():
                          
